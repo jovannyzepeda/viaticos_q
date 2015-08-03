2dd1b5b9e7e04e350da351c3656749cd5e702b7c
Jovanny Zepeda <zepedaroque@hotmail.com>
2015-08-02 19:27:38 -0500

    cambios errores

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723214858) do
+ActiveRecord::Schema.define(version: 20150723213120) do
 
   create_table "brokers", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
